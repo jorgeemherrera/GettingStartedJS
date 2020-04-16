cd42039ccd4100e16f9b073231d1687d3b5a641e
Jorge Eliecer <jorge.m.h@outlook.com>
2020-04-16 11:55:16 -0500

    Objects and the dom module

--- home.js
+++ home.js
@@ -15,10 +15,10 @@ let saved = true;
 saved = !saved;
 
 let savedNull;
-let person = {
-  firstName: 'Jorge',
-  lastName: 'Muñoz'
-};
+// let person = {
+//   firstName: 'Jorge',
+//   lastName: 'Muñoz'
+// };
 
 // showMessage(typeof person);
 // console.log(savedNull);
@@ -31,7 +31,7 @@ if (1 == "1") {
 
 let message = (price > 10) ? 'expensive' : 'cheap';
 
-showMessage(message);
+//showMessage(message);
 
 // for (let i = 0; i < 5; i++) {
 //   console.log(i)
@@ -52,4 +52,51 @@ do {
 while (count < 5);
 
 changePercentOff(30);
+
+let mySymbol = Symbol();
+
+let person = {
+  name: 'Jorge',
+  age: 29,
+  parTime: false,
+  [mySymbol]: 'secretInformation',
+  showInfo: function(realAge) {
+    showMessage(this.name + ' is ' + realAge);
+  }
+}
+
+function incrementAge(person) {
+  person.age++;
+}
+
+incrementAge(person)
+
+console.log(person.age);
+
+console.log(typeof person.showInfo)
+
+person.showInfo(30);
+
+const header = document.getElementById('message');
+header.style.color = 'red';
+header.style.fontWeight = '100';
+
+const button = document.getElementById('see-review');
+
+button.addEventListener('click', function() {
+  //console.log('click');
+  const review = document.getElementById('review');
+
+  //Toggle 
+
+  if (review.classList.contains('d-none')) {
+    review.classList.remove('d-none');
+    button.textContent = 'CLOSE REVIEW';
+  } else {
+    review.classList.add('d-none');
+    button.textContent = 'SEE REVIEW';
+  }
+ 
+})
+
 
